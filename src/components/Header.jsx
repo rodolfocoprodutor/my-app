ab25449aa933f88802854b8b9db4b39d5d3ad30f
Rodolfo Coprodutor <rodolfocoprodutor@gmail.com>
2022-07-23 14:26:30 -0300

    primeiro pull

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+export function Header() {
+    return (
+        <header>
+            <h1>Meu App</h1>
+            <strong>Meu primeiro app</strong>
+        </header>
+    )
+}
---
+++ src/components/Posts.jsx
@@ -0,0 +1,30 @@
+export function Posts() {
+    return (
+        <ul>
+            <li>
+                <h2>Titulo do post</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquid explicabo natus debitis, adipisci dignissimos, consequatur sunt unde voluptates commodi, amet neque deleniti eligendi labore. Cumque dolorem delectus ad tenetur?
+                </p>
+            </li>
+            <li>
+                <h2>Titulo do post</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquid explicabo natus debitis, adipisci dignissimos, consequatur sunt unde voluptates commodi, amet neque deleniti eligendi labore. Cumque dolorem delectus ad tenetur?
+                </p>
+            </li>
+            <li>
+                <h2>Titulo do post</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquid explicabo natus debitis, adipisci dignissimos, consequatur sunt unde voluptates commodi, amet neque deleniti eligendi labore. Cumque dolorem delectus ad tenetur?
+                </p>
+            </li>
+            <li>
+                <h2>Titulo do post</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquid explicabo natus debitis, adipisci dignissimos, consequatur sunt unde voluptates commodi, amet neque deleniti eligendi labore. Cumque dolorem delectus ad tenetur?
+                </p>
+            </li>
+        </ul>
+    )
+}
